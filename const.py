2dad4de212ff9445a1b24ce7bbf2ce0895eca604
Kyle Wickens <kylewickens@gmail.com>
2017-03-26 21:48:36 +0100

    Added constants

---
+++ const.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# Helps define a constant in Python
+# From Section 6.3: Defining Constants
+# Python Cookbook (2 Ed) by Alex Martelli et al.
+#
+# Usage:
+# import const
+# const.magic = 23 # First binding is fine
+# const.magic = 88 # Second binding raises const.ConstError
+
+
+class _const:
+
+    class ConstError(TypeError):
+        pass
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise self.ConstError("Can't rebind const(%s)" % name)
+        self.__dict__[name] = value
+
+
+import sys
+sys.modules[__name__] = _const()
